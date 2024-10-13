3c5062c378fab4066d5c1e9aad54c2e989077dfb
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-10-13 14:06:03 +0600

    updated

--- part1-oodb/create commands.sql
+++ part1-oodb/create commands.sql
@@ -1,29 +1,95 @@
-CREATE OR REPLACE TYPE CustomerName AS OBJECT (
+-- ===============================Customers===================================
+CREATE OR REPLACE TYPE CustomerName_O AS OBJECT (
     firstName VARCHAR2(100),
     lastName VARCHAR2(100)
 );
 /
-DESC CustomerName;
+DESC CustomerName_O;
 /
-
-CREATE OR REPLACE TYPE Address AS OBJECT (
+CREATE OR REPLACE TYPE Address_O AS OBJECT (
     street VARCHAR2(100),
     city VARCHAR2(100),
     zip VARCHAR2(10) 
 );
 /
-DESC Address;
+DESC Address_O;
 /
-
-CREATE TABLE customers (
-    name CustomerName,
-    address Address,
+CREATE OR REPLACE TYPE Customers_O AS OBJECT(
+    customer_id NUMBER ,
+    name CustomerName_O,
+    address Address_O,
     phoneNumber VARCHAR2(15),
     email VARCHAR2(100)
 );
 /
-
+DESC Customers_O;
+/
+CREATE TABLE customers OF Customers_O  (
+    CONSTRAINT pk_customers PRIMARY KEY (customer_id)
+);
+/
 desc customers;
 /
+-- ===============================Restaurants===================================
+CREATE OR REPLACE TYPE Restaurants_O AS OBJECT(
+    restaurant_id NUMBER ,
+    name VARCHAR2(100),
+    address Address_O,
+    rating NUMBER(2,1) 
+);
+/
+DESC Restaurants_O;
+/
 
-select c.name.firstName, c.name.lastName, c.address.street, c.address.city, c.address.zip, phoneNumber, email from customers c;
+CREATE TABLE restaurants OF Restaurants_O  (
+    CONSTRAINT pk_restaurants PRIMARY KEY (restaurant_id)
+);
+/
+desc restaurants;
+/
+-- ===============================Food Items===================================
+CREATE OR REPLACE TYPE FoodItems_O AS OBJECT(
+    food_item_id NUMBER ,
+    name VARCHAR2(100),
+    price NUMBER(8, 2),
+    restaurant_id NUMBER    
+);
+/
+DESC FoodItems_O;
+/
+CREATE TABLE food_items OF FoodItems_O (
+    CONSTRAINT pk_food_items PRIMARY KEY (food_item_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+);
+/
+desc food_items;
+/
+-- ===============================Orders===================================
+CREATE OR REPLACE TYPE FoodItem_O AS OBJECT(
+    id NUMBER,
+    name VARCHAR2(100),
+    price NUMBER(8,2)
+);
+/
+
+CREATE OR REPLACE TYPE DeliveryPersonnel_O AS OBJECT(
+    id NUMBER,
+    name CustomerName_O,
+    phoneNumber VARCHAR2(15)
+);
+/
+
+
+
+
+
+
+
+CREATE OR REPLACE TYPE Orders_O AS OBJECT(
+    id NUMBER,
+    orderDate DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(id));
+    foodItems FoodItemList_T,   -- List of food items (to be defined)
+    deliveryPersonnel REF DeliveryPersonnel_O -- Delivery personnel relationship
+);
+/
--- part1-oodb/insert-commands.sql
+++ part1-oodb/insert-commands.sql
@@ -1,36 +1,114 @@
-INSERT INTO customers (name, address, phoneNumber, email) VALUES (
-    new CustomerName('saifur', 'rahman'), 
-    new Address('kathalbagan', 'dhaka', '1205'), 
+-- ===============================Customers===================================
+INSERT INTO customers (customer_id, name, address, phoneNumber, email) VALUES (
+    1, 
+    new CustomerName_O('saifur', 'rahman'), 
+    new Address_O('kathalbagan', 'dhaka', '1205'), 
     '01703188752', 
     'saifur.rahman1193@gmail.com'
 );
-
-INSERT INTO customers (name, address, phoneNumber, email) VALUES (
-    new CustomerName('riaj', 'ahmed'), 
-    new Address('rampura', 'dhaka', '1215'), 
+INSERT INTO customers (customer_id, name, address, phoneNumber, email) VALUES (
+    2, 
+    new CustomerName_O('riaj', 'ahmed'), 
+    new Address_O('rampura', 'dhaka', '1215'), 
     '01703188753', 
     'riaj.adhmed@gmail.com'
 );
-
-INSERT INTO customers (name, address, phoneNumber, email) VALUES (
-    new CustomerName('rakib', 'ahmed'), 
-    new Address('tongi', 'dhaka', '1225'), 
+INSERT INTO customers (customer_id, name, address, phoneNumber, email) VALUES (
+    3, 
+    new CustomerName_O('rakib', 'ahmed'), 
+    new Address_O('tongi', 'dhaka', '1225'), 
     '01703188754', 
     'rakib.adhmed@gmail.com'
 );
-
-INSERT INTO customers (name, address, phoneNumber, email) VALUES (
-    new CustomerName('rahmatullah', 'ahmed'), 
-    new Address('tongi', 'dhaka', '1225'), 
+INSERT INTO customers (customer_id, name, address, phoneNumber, email) VALUES (
+    4, 
+    new CustomerName_O('rahmatullah', 'ahmed'), 
+    new Address_O('tongi', 'dhaka', '1225'), 
     '01703188755', 
     'rahmatullah.adhmed@gmail.com'
 );
-
-INSERT INTO customers (name, address, phoneNumber, email) VALUES (
-    new CustomerName('tonmoy', 'ahmed'), 
-    new Address('gazipur', 'dhaka', '1325'), 
+INSERT INTO customers (customer_id, name, address, phoneNumber, email) VALUES (
+    5, 
+    new CustomerName_O('tonmoy', 'ahmed'), 
+    new Address_O('gazipur', 'dhaka', '1325'), 
     '01703188755', 
     'tonmoy.adhmed@gmail.com'
 );
+select customer_id, concat(t.name.firstName, t.name.lastName) name, t.address.street street, t.address.city city, t.address.zip zip, phoneNumber, email from customers t;
+-- ===============================Restaurants===================================
+INSERT INTO restaurants (restaurant_id, name, address, rating) VALUES (
+    1,
+    'Chillox', 
+    new Address_O('panthapath', 'dhaka', '1205'), 
+    4.5
+);
+INSERT INTO restaurants (restaurant_id, name, address, rating) VALUES (
+    2,
+    'Burger king', 
+    new Address_O('panthapath', 'dhaka', '1205'), 
+    4.8
+);
+INSERT INTO restaurants (restaurant_id, name, address, rating) VALUES (
+    3,
+    'Burger express', 
+    new Address_O('panthapath', 'dhaka', '1205'), 
+    4.2
+);
+INSERT INTO restaurants (restaurant_id, name, address, rating) VALUES (
+    4,
+    'Pizza Express', 
+    new Address_O('panthapath', 'dhaka', '1205'), 
+    4.7
+);
+INSERT INTO restaurants (restaurant_id, name, address, rating) VALUES (
+    5,
+    'Star Khabab', 
+    new Address_O('panthapath', 'dhaka', '1205'), 
+    4.9
+);
+select restaurant_id, name, t.address.street street, t.address.city city, t.address.zip zip, rating from restaurants t;
+-- ===============================Food Items===================================
+INSERT INTO food_items (food_item_id, name, price, restaurant_id) VALUES (
+    1, 
+    'Pizza', 
+    9.99, 
+    1
+);
+INSERT INTO food_items (food_item_id, name, price, restaurant_id) VALUES (
+    2, 
+    'Cashew Nut Salad', 
+    10.99, 
+    1
+);
+INSERT INTO food_items (food_item_id, name, price, restaurant_id) VALUES (
+    3, 
+    'Kacchi', 
+    7.49, 
+    1
+);
+INSERT INTO food_items (food_item_id, name, price, restaurant_id) VALUES (
+    4, 
+    'Biriyani', 
+    12.99, 
+    2
+);
+INSERT INTO food_items (food_item_id, name, price, restaurant_id) VALUES (
+    5, 
+    'Jorda', 
+    12.99, 
+    2
+);
+SELECT 
+    f.food_item_id,
+    f.name AS food_item_name,
+    f.price,
+    r.name AS restaurant_name,
+    r.address.street AS street,
+    r.address.city AS city,
+    r.address.zip AS zip,
+    r.rating
+FROM 
+    food_items f
+LEFT JOIN restaurants r ON r.restaurant_id = f.restaurant_id;
 
-select c.name.firstName, c.address.city, phoneNumber, email from customers c;
+
